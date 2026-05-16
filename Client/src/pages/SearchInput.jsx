cb088cd2c40cee26fb866e61acbbe6d241c751d7
Gabriel Letto <158781940+DevLetto@users.noreply.github.com>
2026-05-16 20:43:09 -0300

    1

--- Client/src/pages/SearchInput.jsx
+++ Client/src/pages/SearchInput.jsx
@@ -4,15 +4,15 @@ import Sugestion from "./Sugestion";
 export default function SearchInput({ city, setCity, getWeather }) {
   return (
     <div className=" flex flex-col ">
-      <div className="relative">
-          <input
-            type="text"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            className="w-65 h-8 bg-white pl-2 pr-7 truncate rounded font-[Arial]"
-            placeholder="Enter a city name..."
-          />
-          <Search onClick={getWeather} className="absolute bottom-1 left-58" />
+      <div className="relative border">
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          className="w-65 h-8 bg-white pl-2 pr-7 truncate rounded font-[Arial] "
+          placeholder="Enter a city name..."
+        />
+        <Search onClick={getWeather} className="absolute bottom-1 left-58" />
       </div>
       <div>
         <Sugestion city={city} />
