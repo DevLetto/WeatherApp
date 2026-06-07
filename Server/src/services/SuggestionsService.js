ef95a30c459f4b37795125bf8375471309dd7f3c
DevLetto <gabriellettosantos@gmail.com>
2026-06-07 17:56:00 -0300

    Forgot what i did

--- Server/src/services/SuggestionsService.js
+++ Server/src/services/SuggestionsService.js
@@ -1,7 +1,7 @@
 
 export default async function SuggestionsService(city){
      try{
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=0&appid=${process.env.API_KEY}`)
+        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${process.env.API_KEY}`)
 
         const data = await response.json();
 
